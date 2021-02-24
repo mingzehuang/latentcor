714472dccea1c0776d8c371eb8f02426558377fb
Huang <sharkmanhmz@tamu.edu>
2021-02-23 18:14:53 -0600

    Roughly finish generating values for interpolation.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -130,38 +130,89 @@ BBipolgrid <- list(tau, d1, d2)
 BBipol <- chebpol::ipol(BBvalue, grid = BBipolgrid, method = "multilin")
 
 
-# # For NN Case
-# # grid values that used to create precomputed values
-# # d1 <- d2 <-
-# # tau <-
-#
-# # create grid input for ipol
-# NNipolgrid <- list(tau, d1, d2)
-#
-# # interpolation.
-# NNipol <- chebpol::ipol(NNvalue, grid = NNipolgrid, method = "multilin")
-#
+# For NN Case
+# grid values that used to create precomputed values
+tau_grid <- seq(-0.99, 0.99, by = 0.01) # "by" increased from 0.005 to 0.01.
+d11_grid <- d12_grid <- d21_grid <- d22_grid <- seq(0.01, 0.99, length.out = 50)
+NNvalue <- array(NA, c(length(tau_grid), length(d11_grid), length(d12_grid), length(d21_grid), length(d22_grid)))
+
+for (i in 1:length(tau_grid)) {
+  for (j in 1:length(d11_grid)) {
+    for (k in j:length(d12_grid)) {
+      for (l in 1:length(d21_grid)) {
+        for (m in l:length(d22_grid)) {
+          tau <- tau_grid[i]; d1 <- c(d11_grid[j], d12_grid[k]); d2 <- c(d21_grid[l], d22_grid[m])
+          f1 <- function(r)(bridgeF_nn(r, zratio1 = d1, zratio2 = d2) - tau)^2
+          op <- tryCatch(optimize(f1, lower = -0.99, upper = 0.99, tol = tol)[1], error = function(e) 100)
+          if(op == 100) {
+            warning("Optimize returned error one of the pairwise correlations, returning NA")
+            NNvalue[i, j, k, l, m] <- NA
+          } else {
+            NNvalue[i, j, k, l, m] <- unlist(op)
+          }
+        }
+      }
+    }
+  }
+}
+# create grid input for ipol
+NNipolgrid <- list(tau_grid, d11_grid, d12_grid, d21_grid, d22_grid)
+# interpolation.
+NNipol <- chebpol::ipol(NNvalue, grid = NNipolgrid, method = "multilin")
+
 # # For NB Case
-# # grid values that used to create precomputed values
-# # d1 <-
-# # d2 <- seq(0.01, 0.99, length.out = 50)
-# # tau <-
-#
-# # create grid input for ipol
-# NBipolgrid <- list(tau, d1, d2)
-#
-# # interpolation.
-# NBipol <- chebpol::ipol(NBvalue, grid = NBipolgrid, method = "multilin")
+# grid values that used to create precomputed values
+tau_grid <- seq(-0.99, 0.99, by = 0.01) # "by" increased from 0.005 to 0.01.
+d11_grid <- d12_grid <- d2_grid <- seq(0.01, 0.99, length.out = 50)
+NBvalue <- array(NA, c(length(tau_grid), length(d11_grid), length(d12_grid), length(d2_grid)))
+
+for (i in 1:length(tau_grid)) {
+  for (j in 1:length(d11_grid)) {
+    for (k in j:length(d12_grid)) {
+      for (l in 1:length(d2_grid)) {
+          tau <- tau_grid[i]; d1 <- c(d11_grid[j], d12_grid[k]); d2 <- d2_grid[l]
+          f1 <- function(r)(bridgeF_nb(r, zratio1 = d1, zratio2 = d2) - tau)^2
+          op <- tryCatch(optimize(f1, lower = -0.99, upper = 0.99, tol = tol)[1], error = function(e) 100)
+          if(op == 100) {
+            warning("Optimize returned error one of the pairwise correlations, returning NA")
+            NBvalue[i, j, k, l] <- NA
+          } else {
+            NBvalue[i, j, k, l] <- unlist(op)
+          }
+      }
+    }
+  }
+}
+# create grid input for ipol
+NBipolgrid <- list(tau_grid, d11_grid, d12_grid, d2_grid)
+# interpolation.
+NBipol <- chebpol::ipol(NBvalue, grid = NBipolgrid, method = "multilin")
 #
 # # For NC Case
-# # d1 <-
-# # tau <-
-#
-# # create grid input for ipol
-# NCipolgrid <- list(tau, d1)
-#
-# # interpolation.
-# NCipol <- chebpol::ipol(NCvalue, grid = NCipolgrid, method = "multilin")
+# grid values that used to create precomputed values
+tau_grid <- seq(-0.99, 0.99, by = 0.01) # "by" increased from 0.005 to 0.01.
+d11_grid <- d12_grid <- seq(0.01, 0.99, length.out = 50)
+NCvalue <- array(NA, c(length(tau_grid), length(d11_grid), length(d12_grid)))
+
+for (i in 1:length(tau_grid)) {
+  for (j in 1:length(d11_grid)) {
+    for (k in j:length(d12_grid)) {
+        tau <- tau_grid[i]; d1 <- c(d11_grid[j], d12_grid[k])
+        f1 <- function(r)(bridgeF_nc(r, zratio1 = d1) - tau)^2
+        op <- tryCatch(optimize(f1, lower = -0.99, upper = 0.99, tol = tol)[1], error = function(e) 100)
+        if(op == 100) {
+          warning("Optimize returned error one of the pairwise correlations, returning NA")
+          NCvalue[i, j, k] <- NA
+        } else {
+          NCvalue[i, j, k] <- unlist(op)
+        }
+    }
+  }
+}
+# create grid input for ipol
+NCipolgrid <- list(tau_grid, d11_grid, d12_grid)
+# interpolation.
+NCipol <- chebpol::ipol(NCvalue, grid = NCipolgrid, method = "multilin")
 
 usethis::use_data(TCipol, TTipol, TBipol, BCipol, BBipol, internal = TRUE, overwrite = TRUE, compress = "xz")
 
