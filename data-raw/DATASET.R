c5a6c2bfb4ddb66f0d94ad7e548b1f6d14fd453e
Huang <sharkmanhmz@tamu.edu>
2021-03-02 12:15:41 -0600

    Generate all data inside this file without importing data files.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -56,7 +56,7 @@ for (i in 1:length(tau_grid)) {
 # create grid input for ipol
 TCipolgrid <- list(tau_grid, d1_grid)
 # interpolation.
-NBipol <- chebpol::ipol(TCvalue, grid = TCipolgrid, method = "multilin")
+TCipol <- chebpol::ipol(TCvalue, grid = TCipolgrid, method = "multilin")
 
 
 
@@ -64,97 +64,202 @@ NBipol <- chebpol::ipol(TCvalue, grid = TCipolgrid, method = "multilin")
 # For TT case
 ############################################################################################
 
-load("~/Dropbox/TAMU/Irina/mixedCCAfast/R1_PrecomputedResults/tt_0804.Rda")
+# load("~/Dropbox/TAMU/Irina/mixedCCAfast/R1_PrecomputedResults/tt_0804.Rda")
+#
+# # grid values that used to create precomputed values.
+# # d2 <- log10(seq(1, 10^0.99, length = 50))
+# # tau <- seq(-0.99, 0.99, by = 0.01) # "by" increased from 0.005 to 0.01.
+#
+# TTvalue <- array(NA, dim = c(length(tau), length(d1), length(d2)))
+# for (i in 1:length(d1)){
+#   for ( j in 1:length(d2)){
+#     for ( k in 1:length(tau)){
+#       TTvalue[k, i, j] <- gridTTinv[[length(d2)*(i - 1) + j]][k]
+#     }
+#   }
+# }
+#
+# # create grid input for ipol
+# TTipolgrid <- list(tau, d1, d2)
+#
+# # interpolation.
+# TTipol <- chebpol::ipol(TTvalue, grid = TTipolgrid, method = "multilin")
 
-# grid values that used to create precomputed values.
-# d2 <- log10(seq(1, 10^0.99, length = 50))
-# tau <- seq(-0.99, 0.99, by = 0.01) # "by" increased from 0.005 to 0.01.
+# For TT Case
+# grid values that used to create precomputed values
+tau_grid <- seq(-0.99, 0.99, by = 0.01) # "by" increased from 0.005 to 0.01.
+d1_grid <- d2_grid <- log10(seq(1, 10^0.99, length = 50))
+TTvalue <- array(NA, c(length(tau_grid), length(d1_grid), length(d2_grid)))
 
-TTvalue <- array(NA, dim = c(length(tau), length(d1), length(d2)))
-for (i in 1:length(d1)){
-  for ( j in 1:length(d2)){
-    for ( k in 1:length(tau)){
-      TTvalue[k, i, j] <- gridTTinv[[length(d2)*(i - 1) + j]][k]
+for (i in 1:length(tau_grid)) {
+  for (j in 1:length(d1_grid)) {
+    for (k in j:length(d2_grid)) {
+          tau <- tau_grid[i]; d1 <- d1_grid[j]; d2 <- d2_grid[k]
+          f1 <- function(r)(bridgeF_tt(r, zratio1 = d1, zratio2 = d2) - tau)^2
+          op <- tryCatch(optimize(f1, lower = -0.99, upper = 0.99, tol = 1e-3)[1], error = function(e) 100)
+          if(op == 100) {
+            warning("Optimize returned error one of the pairwise correlations, returning NA")
+            TTvalue[i, j, k] <- NA
+          } else {
+            TTvalue[i, j, k] <- unlist(op)
+          }
     }
   }
 }
-
 # create grid input for ipol
-TTipolgrid <- list(tau, d1, d2)
-
+TTipolgrid <- list(tau_grid, d1_grid, d2_grid)
 # interpolation.
 TTipol <- chebpol::ipol(TTvalue, grid = TTipolgrid, method = "multilin")
 
 ############################################################################################
 # For TB case
 ############################################################################################
-load("~/Dropbox/TAMU/Irina/mixedCCAfast/R1_PrecomputedResults/tb_0817.Rda")
+# load("~/Dropbox/TAMU/Irina/mixedCCAfast/R1_PrecomputedResults/tb_0817.Rda")
+#
+# # grid values that used to create precomputed values
+# # d1 <- log10(seq(1, 10^0.99, length = 50))
+# # d2 <- seq(0.01, 0.99, length.out = 50)
+# # tau1 <- c(seq(-0.5, -0.1, by = 0.007), seq(-0.095, -0.001, by = 0.005))
+# # tau <- c(tau1, 0, rev(-tau1))
+#
+# TBvalue <- array(NA, dim = c(length(tau), length(d1), length(d2)))
+# for (i in 1:length(d1)){
+#   for ( j in 1:length(d2)){
+#     for ( k in 1:length(tau)){
+#       TBvalue[k, i, j] <- gridTBinv[[length(d2)*(i - 1) + j]][k]
+#     }
+#   }
+# }
+#
+# # create grid input for ipol
+# TBipolgrid <- list(tau, d1, d2)
+#
+# # interpolation.
+# TBipol <- chebpol::ipol(TBvalue, grid = TBipolgrid, method = "multilin")
 
+# For TB Case
 # grid values that used to create precomputed values
-# d1 <- log10(seq(1, 10^0.99, length = 50))
-# d2 <- seq(0.01, 0.99, length.out = 50)
-# tau1 <- c(seq(-0.5, -0.1, by = 0.007), seq(-0.095, -0.001, by = 0.005))
-# tau <- c(tau1, 0, rev(-tau1))
+tau1_grid <- c(seq(-0.5, -0.1, by = 0.007), seq(-0.095, -0.001, by = 0.005))
+tau_grid <- c(tau1, 0, rev(-tau1))
+d1_grid <- log10(seq(1, 10^0.99, length = 50))
+d2_grid <- seq(0.01, 0.99, length.out = 50)
+TBvalue <- array(NA, c(length(tau_grid), length(d1_grid), length(d2_grid)))
 
-TBvalue <- array(NA, dim = c(length(tau), length(d1), length(d2)))
-for (i in 1:length(d1)){
-  for ( j in 1:length(d2)){
-    for ( k in 1:length(tau)){
-      TBvalue[k, i, j] <- gridTBinv[[length(d2)*(i - 1) + j]][k]
+for (i in 1:length(tau_grid)) {
+  for (j in 1:length(d1_grid)) {
+    for (k in j:length(d2_grid)) {
+      tau <- tau_grid[i]; d1 <- d1_grid[j]; d2 <- d2_grid[k]
+      f1 <- function(r)(bridgeF_tb(r, zratio1 = d1, zratio2 = d2) - tau)^2
+      op <- tryCatch(optimize(f1, lower = -0.99, upper = 0.99, tol = 1e-3)[1], error = function(e) 100)
+      if(op == 100) {
+        warning("Optimize returned error one of the pairwise correlations, returning NA")
+        TBvalue[i, j, k] <- NA
+      } else {
+        TBvalue[i, j, k] <- unlist(op)
+      }
     }
   }
 }
-
 # create grid input for ipol
-TBipolgrid <- list(tau, d1, d2)
-
+TBipolgrid <- list(tau_grid, d1_grid, d2_grid)
 # interpolation.
 TBipol <- chebpol::ipol(TBvalue, grid = TBipolgrid, method = "multilin")
 
 ############################################################################################
 # For BC case
 ############################################################################################
-load("~/Dropbox/TAMU/Irina/mixedCCAfast/R1_PrecomputedResults/bc_0817.Rda")
+# load("~/Dropbox/TAMU/Irina/mixedCCAfast/R1_PrecomputedResults/bc_0817.Rda")
+#
+# # grid values that used to create precomputed values
+# # d1 <- seq(0.01, 0.99, length.out = 50)
+# # tau1 <- c(seq(-0.5, -0.1, by = 0.007), seq(-0.095, -0.001, by = 0.005))
+# # tau <- c(tau1, 0, rev(-tau1))
+#
+# # create input values for ipol
+# BCvalue <- matrix(unlist(gridBCinv), ncol = length(d1), byrow = FALSE)
+#
+# # create grid input for ipol
+# BCipolgrid <- list(tau, d1)
+#
+# # interpolation
+# BCipol <- chebpol::ipol(BCvalue, grid = BCipolgrid, method = "multilin")
 
+# For BC Case
 # grid values that used to create precomputed values
-# d1 <- seq(0.01, 0.99, length.out = 50)
-# tau1 <- c(seq(-0.5, -0.1, by = 0.007), seq(-0.095, -0.001, by = 0.005))
-# tau <- c(tau1, 0, rev(-tau1))
+tau1_grid <- c(seq(-0.5, -0.1, by = 0.007), seq(-0.095, -0.001, by = 0.005))
+tau_grid <- c(tau1, 0, rev(-tau1))
+d1_grid <- seq(0.01, 0.99, length.out = 50)
+BCvalue <- matrix(NA, length(tau_grid), length(d1_grid))
 
-# create input values for ipol
-BCvalue <- matrix(unlist(gridBCinv), ncol = length(d1), byrow = FALSE)
-
+for (i in 1:length(tau_grid)) {
+  for (j in 1:length(d1_grid)) {
+      tau <- tau_grid[i]; d1 <- d1_grid[j]
+      f1 <- function(r)(bridgeF_bc(r, zratio1 = d1) - tau)^2
+      op <- tryCatch(optimize(f1, lower = -0.99, upper = 0.99, tol = 1e-3)[1], error = function(e) 100)
+      if(op == 100) {
+        warning("Optimize returned error one of the pairwise correlations, returning NA")
+        BCvalue[i, j] <- NA
+      } else {
+        BCvalue[i, j] <- unlist(op)
+      }
+  }
+}
 # create grid input for ipol
-BCipolgrid <- list(tau, d1)
-
-# interpolation
+BCipolgrid <- list(tau_grid, d1_grid)
+# interpolation.
 BCipol <- chebpol::ipol(BCvalue, grid = BCipolgrid, method = "multilin")
 
 ############################################################################################
 # For BB case
 ############################################################################################
-load("~/Dropbox/TAMU/Irina/mixedCCAfast/R1_PrecomputedResults/bb_0817.Rda")
+# load("~/Dropbox/TAMU/Irina/mixedCCAfast/R1_PrecomputedResults/bb_0817.Rda")
+#
+# # grid values that used to create precomputed values
+# # d1 <- d2 <- seq(0.01, 0.99, length.out = 50)
+# # tau1 <- c(seq(-0.5, -0.1, by = 0.007), seq(-0.095, -0.001, by = 0.005))
+# # tau <- c(tau1, 0, rev(-tau1))
+#
+# BBvalue <- array(NA, dim = c(length(tau), length(d1), length(d2)))
+# for (i in 1:length(d1)){
+#   for ( j in 1:length(d2)){
+#     for ( k in 1:length(tau)){
+#       BBvalue[k, i, j] <- gridBBinv[[length(d2)*(i - 1) + j]][k]
+#     }
+#   }
+# }
+#
+# # create grid input for ipol
+# BBipolgrid <- list(tau, d1, d2)
+#
+# # interpolation.
+# BBipol <- chebpol::ipol(BBvalue, grid = BBipolgrid, method = "multilin")
 
+# For BB Case
 # grid values that used to create precomputed values
-# d1 <- d2 <- seq(0.01, 0.99, length.out = 50)
-# tau1 <- c(seq(-0.5, -0.1, by = 0.007), seq(-0.095, -0.001, by = 0.005))
-# tau <- c(tau1, 0, rev(-tau1))
+tau1_grid <- c(seq(-0.5, -0.1, by = 0.007), seq(-0.095, -0.001, by = 0.005))
+tau_grid <- c(tau1, 0, rev(-tau1))
+d1_grid <- d2_grid <- seq(0.01, 0.99, length.out = 50)
+BBvalue <- matrix(NA, length(tau_grid), length(d1_grid), length(d2_grid))
 
-BBvalue <- array(NA, dim = c(length(tau), length(d1), length(d2)))
-for (i in 1:length(d1)){
-  for ( j in 1:length(d2)){
-    for ( k in 1:length(tau)){
-      BBvalue[k, i, j] <- gridBBinv[[length(d2)*(i - 1) + j]][k]
+for (i in 1:length(tau_grid)) {
+  for (j in 1:length(d1_grid)) {
+    for (k in 1:length(d2_grid)) {
+      tau <- tau_grid[i]; d1 <- d1_grid[j]; d2 <- d2_grid[k]
+      f1 <- function(r)(bridgeF_bb(r, zratio1 = d1, zratio2 = d2) - tau)^2
+      op <- tryCatch(optimize(f1, lower = -0.99, upper = 0.99, tol = 1e-3)[1], error = function(e) 100)
+      if(op == 100) {
+        warning("Optimize returned error one of the pairwise correlations, returning NA")
+        BBvalue[i, j, k] <- NA
+      } else {
+        BBvalue[i, j, k] <- unlist(op)
+      }
     }
   }
 }
-
 # create grid input for ipol
-BBipolgrid <- list(tau, d1, d2)
-
+BBipolgrid <- list(tau_grid, d1_grid, d2_grid)
 # interpolation.
 BBipol <- chebpol::ipol(BBvalue, grid = BBipolgrid, method = "multilin")
-
 
 # For NN Case
 # grid values that used to create precomputed values
