1e436cf1234d5a1517df337381b164c775f50438
Huang <sharkmanhmz@tamu.edu>
2021-03-15 10:03:30 -0500

    Make example code more compact.

--- R/PlotData.R
+++ R/PlotData.R
@@ -1,16 +1,16 @@
 #' Plot true correlation vs estimated correlation
 #' \code{PlotCompare} is to check unbiasness of estimation by plotting true correlation from simulation data vs estimated correlation from simulation data.
-#' @param data1 First data series.
-#' @param data2 Second data series.
-#' @param name1 Name for first data series.
-#' @param name2 Name for second data series.
+#' @param pairlist list for data pairs.
+#' @param namelist list for names of data pairs.
 #' @import ggplot2
 #' @return \code{PlotCompare} returns a plot for data1 against data2 and 45 degree benchmark line.
 #' @example man/examples/estimateR_ex.R
 #' @export
-PlotCompare <- function(data1, data2, name1, name2) {
-df <- data.frame(c(data1), c(data2))
-colnames(df) = c(name1, name2)
-  out <- ggplot(df, aes(df[paste(name1)], df[paste(name2)]), color = "blue") + geom_point() + geom_abline(intercept = 0, slope = 1, color="red")
-return(out)
+PlotCompare <- function(pairlist, namelist) {
+  l_list <- length(pairlist)
+  for (i in 1:l_list) {
+    df <- data.frame(pairlist[[i]])
+    colnames(df) = namelist[[i]]
+    ggplot(df, aes(df[paste(namelist[[i]][1])], df[paste(namelist[[i]][2])]), color = "blue") + geom_point() + geom_abline(intercept = 0, slope = 1, color="red")
+  }
 }
