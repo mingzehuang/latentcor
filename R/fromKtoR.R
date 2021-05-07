e4765e1ff51e12064a023858b72ef44e22e0de7c
Mingze Huang <mingzehuang@gmail.com>
2021-05-06 22:49:25 -0500

    Deal with single row zratio input.

--- R/fromKtoR.R
+++ R/fromKtoR.R
@@ -12,7 +12,7 @@ fromKtoR <- function(K, zratio, type, method, tol, ratio) {
     zratio1 = apply(zratio, 2, function(x){rep(x, d1)})
     zratio2 = apply(zratio, 2, function(x){rep(x, each = d1)})
     upperR <- c(upper.tri(K)) # length p^2 of true/false with true corresponding to upper.tri
-    hatRupper = R_sol(type1 = type, type2 = type, tau = c(K[upperR]), zratio1 = as.matrix(zratio1[upperR, ]), zratio2 = as.matrix(zratio2[upperR, ]), method = method, tol = tol, ratio = ratio)
+    hatRupper = R_sol(type1 = type, type2 = type, tau = c(K[upperR]), zratio1 = as.matrix(zratio1[upperR, ], nrow = sum(upperR)), zratio2 = as.matrix(zratio2[upperR, ], nrow = sum(upperR)), method = method, tol = tol, ratio = ratio)
     # Get upperR into hatR
     hatR <- matrix(0, d1, d1)
     hatR[upperR] <- hatRupper
@@ -33,10 +33,10 @@ fromKtoR_mixed <- function(K12, zratio1, zratio2, type1, type2, method, tol, rat
   if ((type1 == "continuous" & type2 == "binary") | (type1 == "continuous" & type2 == "trunc") |
       (type1 == "binary" & type2 == "trunc") | (type1 == "continuous" & type2 == "ternary") |
       (type1 == "binary" & type2 == "ternary")) {
-    hatR = R_sol(type1 = type2, type2 = type1, tau = c(t(K12)), zratio1 = zratio2, zratio2 = zratio1, method = method, tol = tol, ratio = ratio)
+    hatR = R_sol(type1 = type2, type2 = type1, tau = c(t(K12)), zratio1 = as.matrix(zratio2, nrow = length(K12)), as.matrix(zratio2 = zratio1, nrow = length(K12)), method = method, tol = tol, ratio = ratio)
     return(matrix(hatR, d1, d2, byrow = TRUE))
   } else {
-    hatR = R_sol(type1 = type1, type2 = type2, tau = c(K12), zratio1 = zratio1, zratio2 = zratio2, method = method, tol = tol, ratio = ratio)
+    hatR = R_sol(type1 = type1, type2 = type2, tau = c(K12), zratio1 = as.matrix(zratio1, nrow = length(K12)), as.matrix(zratio2 = zratio2, nrow = length(K12)), method = method, tol = tol, ratio = ratio)
     return(matrix(hatR, d1, d2))
   }
 }
