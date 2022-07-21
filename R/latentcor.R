c093e6665885e138b452f0dedc59935fcf2842b6
Mingze (Rico) Huang <mingzehuang@gmail.com>
2022-07-21 12:18:23 -0500

    Make approx as default method.

--- R/latentcor.R
+++ R/latentcor.R
@@ -47,7 +47,7 @@
 #' @export
 #' @example man/examples/latentcor_ex.R
 
-latentcor = function(X, types = NULL, method = c("original", "approx"), use.nearPD = TRUE, nu = 0.001, tol = 1e-8, ratio = 0.9, showplot = FALSE){
+latentcor = function(X, types = NULL, method = c("approx", "original"), use.nearPD = TRUE, nu = 0.001, tol = 1e-8, ratio = 0.9, showplot = FALSE){
   # Check the supplied parameters are compatible with what is expected
   if(nu < 0 | nu > 1){
     stop("nu must be between 0 and 1.")
